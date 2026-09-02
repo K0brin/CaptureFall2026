f46bee704ee74ec359d5d95cc20a81c4d87fa990
Lundy <nlundy@student.uiwtx.edu>
2026-09-02 17:44:59 -0500

    added UI

--- Source/Capture/Private/Player/CPlayerController.h
+++ Source/Capture/Private/Player/CPlayerController.h
@@ -14,4 +14,22 @@ class ACPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+	//only called on server
+	virtual void OnPossess(APawn* NewPawn) override;
+
+	//when client recieved pawn on client machine, not called on dedicated server
+	virtual void AcknowledgePossession(APawn* NewPawn) override;
+
+private:
+	UPROPERTY()
+	class ACPlayerCharacter* CPlayerCharacter;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Widget")
+	TSubclassOf<class UGameplayWidget> GameplayWidgetClass;
+
+	UPROPERTY()
+	UGameplayWidget* GameplayWidget;
+
+	void SpawnGameplayWidget();
 };
